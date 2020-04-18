c38e22a9108ebccca93570a59e08014cc5038ed7
Nigel Christiaans <nigelchristiaans747@gmail.com>
2020-04-18 11:48:13 +0200

    update week4

--- week4.py
+++ week4.py
@@ -1,19 +1,21 @@
-import math
-import time
+import math #Hier word de math library geimporteerd
 
 
-op = ""
+#Dit script is een gemodificeerde versie van mijn calculator https://github.com/nigel12341/python-projects/blob/master/calculator.py samen met week2 en week3 van de python opdrachten
+
+
+op = "" #Komende regels wat variabelen
 num1 = ""
 num2 = ""
 woord = ""
 oppervlaktecalc = ""
 
-def oppervlakte():
-    num1 = int(input("Voer de lengte in"))
-    num2 = int(input("Voer de breedte in"))
-    oppervlaktecalc = num1 * num2
-    print(oppervlaktecalc)
-    calc()
+def oppervlakte(): #De oppervlakte circel
+    num1 = int(input("Voer de lengte in")) #hier kun je het eerste nummer invoeren
+    num2 = int(input("Voer de breedte in")) #hier het 2e nummer
+    oppervlaktecalc = num1 * num2 #Hier word de oppervlakte uitgerekend
+    print(oppervlaktecalc) #Hier word ie geprint
+    calc() # Hier word het calc functie gerund zodat je niet de hele tijd het script opnieuw moet openen
 
 # deze functie heeft drie parameters en 1 uitvoer (het grootste van drie getallen)
 def maxThree(getal1, getal2, getal3): # een functie definen die 3 variablen nodig heeft
@@ -24,7 +26,7 @@ def maxThree(getal1, getal2, getal3): # een functie definen die 3 variablen nodi
        grootste = getal3 # Hier word gezegd dat als getal3 grote is dan grootste dan word getal3 het grootste
     return grootste #Hier word het variable grootste gereturned als de functie word gecalled
 
-def grootstegetal():
+def grootstegetal(): # Dit is de functie voor het grootste getal
     aantal = input("Hoe vaak wil je het maximum van drie getallen berekenen? ") # Hier heb je een input dus kun je iets invullen
     for i in range(0, int(aantal)): # een For loop in een range van 0 tot variable aantal
         getalen1 = input("Het eerste van de drie getallen: ") # Weer een input
@@ -32,45 +34,45 @@ def grootstegetal():
         getalen3 = input("Het derde van de drie getallen:  ") #nog een input
         grootste = maxThree(getalen1 ,getalen2 ,getalen3 ) # hier doen ze de grootste kiezen via een funcite
     print("Het grootste getal van : " + getalen1 + ", " + getalen2 + " en " + getalen3 + " is " + str(grootste)) # Hier printen ze het grootste getal
-    calc()
+    calc() # Hier word het calc functie gerund zodat je niet de hele tijd het script opnieuw moet openen
 
-def aantalletters():
+def aantalletters(): #Dit is de functie voor het aantal letter uitrekenen
     print("Voer een woord in") #Print een string
     Totaalwoord = input("Typ een woord in: ") # een input
     woordLengte = 0 # variable is gelijk aan 0
     for i in Totaalwoord: #een For loop d ie door het variable totaalwoord gaat tot ie alles heeft
         woordLengte += 1 # telt omhoog met 1 per keer
     print("Het totaal aantal letter in het woord: " + Totaalwoord + " is " + str(woordLengte)) # print het totaal aantal letter met variablen
-    calc()
+    calc() # Hier word het calc functie gerund zodat je niet de hele tijd het script opnieuw moet openen
 
-def oppervlaktecirkel():
-    print("Voer de straal van de cirkel in")
-    num1 = int(input())
-    print(math.pi*num1*num1)
-    calc()
+def oppervlaktecirkel(): #Hier word het oppervlakte berekend
+    print("Voer de straal van de cirkel in")#print
+    num1 = int(input())#hier moet je de straal invullne
+    print(math.pi*num1*num1)#hier rekend ie de oppervlakte uit
+    calc() # Hier word het calc functie gerund zodat je niet de hele tijd het script opnieuw moet openen
 
-def inhoudcirkel():
-    print("Voer de straal van de cirkel in")
-    num1 = int(input())
-    num2 = int(input("Voer de hoogte van de circel in"))
-    print(math.pi*num1*num1*num2)
-    calc()
+def inhoudcirkel():#inhoud uitrekende
+    print("Voer de straal van de cirkel in") #print
+    num1 = int(input()) #het eerste nummer 
+    num2 = int(input("Voer de hoogte van de circel in")) # het 2e nummer
+    print(math.pi*num1*num1*num2) #rekend de inhoud uit
+    calc() # Hier word het calc functie gerund zodat je niet de hele tijd het script opnieuw moet openen
 
-def calc():
-    print("Welkom!")
-    print("Wat wil je doen? oppervlakte, grootste getal, aantal letters, oppervlakte cirkel, inhoud cilinder")
-    op = input("")
-    if op == "oppervlakte":
-        oppervlakte()
-    elif op == "grootste getal":
-         grootstegetal()
-    elif op == "aantal letters":
-        aantalletters()
-    elif op == "oppervlakte cirkel":
-        oppervlaktecirkel()
-    elif op == "inhoud cilinder":
-        inhoudcirkel()
-    else:
+def calc(): #de main functie
+    print("Welkom!")#print
+    print("Wat wil je doen? oppervlakte, grootste getal, aantal letters, oppervlakte cirkel, inhoud cilinder") #print
+    op = input("")# hier moet je kiezen uit oppervlakte, grootste getal, aantal letters, oppervlakte cirkel, inhoud cilinder
+    if op == "oppervlakte": #if oppervlakte
+        oppervlakte() #de oppervlakte funcite
+    elif op == "grootste getal": #een elif
+         grootstegetal() #de grootste getal funcite
+    elif op == "aantal letters": #een elif
+        aantalletters() # de aantalletters funcite
+    elif op == "oppervlakte cirkel": #een elif
+        oppervlaktecirkel() #de oppervlakte cirkel nummer
+    elif op == "inhoud cilinder": #een elif 
+        inhoudcirkel() #inhoud cirkel funcite
+    else: #als niks werkt word error laten zien
         print("Error")
 
-calc()
+calc() #zodat de calc functie opend
